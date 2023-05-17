692715356c614dc7239a34495c653738697ff81a
Shrinithp <shrinithcourses@gmail.com>
2023-05-17 13:44:02 +0530

    end of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+//these are decorators.
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+//just like we have dependency injection in dotnet file we also have in angular i.e, we have to inject http module.
+
+//constructor is too early to fetch data from an api so lifecycle process is implemented
+export class AppComponent implements OnInit {
+//title can come after constructor or before.
+  title = 'DatingApp'; //title is of type string.
+  users:any;  //user is of type any.
+
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.http.get("https://localhost:5000/api/users").subscribe({
+      next:response => this.users = response,
+      error: error=>console.log(error),
+      complete:() =>console.log("request completed")
+    })
+  }
+
+
+
+  
+
+}
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  //inoder to get data from server and display it in out clent page we have to use httpclient module.
+  //inorder to use this module we have to import this first which is done in line number 3.
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
